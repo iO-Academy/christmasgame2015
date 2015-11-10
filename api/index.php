cb0045c0ca1ecb2f8955e22f318919d15f6ad453
mporam <emailme@mikeoram.co.uk>
2015-11-10 17:43:40 +0000

    update api

--- api/index.php
+++ api/index.php
@@ -5,7 +5,7 @@ $response = array(
     'message' => 'No action passed'
 );
 
-if (!empty($_POST['action']) && strlen($_POST['action']) < 13) {
+if (!empty($_POST['action'])) {
 
     require_once('includes/dbcon.php');
 
@@ -22,10 +22,35 @@ if (!empty($_POST['action']) && strlen($_POST['action']) < 13) {
                 $email = $_POST['userEmail'];
                 // save user in database
                 try {
-                    $query = 'INSERT INTO `users` (`name`, `email`) VALUES ("' . $name . '", "' . $email . '")';
+
+                    $query = 'SELECT `id`, `attempts` FROM `users` WHERE `userEmail` = "' . $email . '";';
+                    $conn = $db->prepare($query);
+                    $conn->execute();
+                    $curUser = $conn->fetch();
+
+                    if (!empty($curUser)) {
+                        $attempts = $curUser['attempts']+1;
+
+                        $id = $curUser['id'];
+                        $_SESSION['id'] = $curUser['id'];
+                        $_SESSION['attempts'] = $attempts;
+
+                        $query = 'UPDATE `users` SET `attempts` = ' . $attempts . ' WHERE `id` = ' . $id . ';';
+                        $db->exec($query);
+
+                        $response = array(
+                            'success' => true,
+                            'message' => 'User account exists',
+                            'curAttempt' => $attempts
+                        );
+                        break;
+                    }
+
+                    $query = 'INSERT INTO `users` (`userName`, `userEmail`) VALUES ("' . $name . '", "' . $email . '")';
                     $db->exec($query);
 
                     $_SESSION['id'] = $db->lastInsertId();// id from database
+                    $_SESSION['try'] = 1;
                     $response = array(
                         'success' => true,
                         'message' => 'User saved'
@@ -33,7 +58,7 @@ if (!empty($_POST['action']) && strlen($_POST['action']) < 13) {
                 } catch (Exception $e) {
                     $response = array(
                         'success' => false,
-                        'message' => 'Duplicate user email'
+                        'message' => $e->getMessage()
                     );
                 }
             }
@@ -44,29 +69,57 @@ if (!empty($_POST['action']) && strlen($_POST['action']) < 13) {
             $attempts = $_POST['attempts'];
             $time = $_POST['time'];
             $user = $_SESSION['id'];
+            $try = $_SESSION['attempts'];
 
-            // save current level details
-            $query = 'INSERT INTO `levels` (`level`, `user`, `attempts`, `time`)
-                        VALUES ("' . $level . '", "' . $user . '", "' . $attempts . '", "' . $time . '")';
-            $db->exec($query);
-
-            $response = array(
-                'success' => true,
-                'message' => 'Level saved'
-            );
-
-            if (5 == $level) {
-                $query = 'SELECT SUM(`time`) FROM `levels` WHERE `user` = ' . $user . ';';
+            try {
+                $query = 'SELECT 1 FROM `levels` WHERE `user` = ' . $user . ' AND `try` = ' . $try . ' AND `level` = ' . $level . ';';
                 $conn = $db->prepare($query);
                 $conn->execute();
-                $totalTime = $conn->fetch();
+                $previousSave = $conn->fetch();
 
-                $response['data'] = $totalTime[0];
-                // add all level times together and save
+                if ($previousSave) {
+                    $response['message'] = 'Error occured: level already saved';
+                    break;
+                }
+
+                // save current level details
+                $query = 'INSERT INTO `levels` (`level`, `user`, `attempts`, `time`, `try`)
+                        VALUES ("' . $level . '", "' . $user . '", "' . $attempts . '", "' . $time . '", "' . $try . '")';
+                $db->exec($query);
+
+                $response = array(
+                    'success' => true,
+                    'message' => 'Level saved'
+                );
+
+                if (5 == $level) {
+                    $query = 'SELECT SUM(`time`) FROM `levels` WHERE `user` = ' . $user . ' AND `try` = ' . $try . ';';
+                    $conn = $db->prepare($query);
+                    $conn->execute();
+                    $totalTime = $conn->fetch();
+
+                    $response['totalTime'] = $totalTime[0];
+                    // add all level times together and save
+                }
+            } catch (Exception $e) {
+                $response = array(
+                    'success' => false,
+                    'message' => 'An unexpected error occurred'
+                );
             }
             break;
         case 'getDetails':
-            // return the users completed game details
+            $user = $_SESSION['id'];
+            $query = 'SELECT `level`, `attempts`, `time`, `try` FROM `levels` WHERE `user` = ' . $user . ' ORDER BY `try`, `level`;';
+            $conn = $db->prepare($query);
+            $conn->execute();
+            $details = $conn->fetchAll(PDO::FETCH_ASSOC);
+
+            $response = array(
+                'success' => true,
+                'message' => 'Success',
+                'details' => json_encode($details)
+            );
             break;
         default:
             $response['message'] = 'Invalid action passed';
