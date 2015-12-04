bfc9e3ca3c848a3e0faeec7d623c119173e85f7b
peter.wilkins.mayden <peter.wilkins@mayden.com>
2015-12-04 16:50:54 +0000

    validation for #userName and #email, splash.css created

--- js/splash.js
+++ js/splash.js
@@ -7,9 +7,25 @@ $(function () {
     $('#target').submit(function (e) {
         e.preventDefault();
 
+        if (!$('#userName').val()) {
+            $("#err1").alert('close');
+            $('#userName').after(
+                '<div class="alert alert-danger alert-dismissable err" id="err1">' +
+                '<button type="button" class="close" ' +
+                'data-dismiss="alert" aria-hidden="true">' +
+                '&times;' +
+                '</button>' +
+                'Required field' +
+                '</div>');
+            $('#err1').slideDown('slow');
+            console.log('field1 is empty');
+        }
+        else {
+            $("#err1").slideUp('close');
+        }
         var testEmail = /[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}/igm;
         if (!testEmail.test($('#email').val())) {
-            $("#err3").alert('close');
+            $("#err2").alert('close');
             $('#email').after(
                 '<div class="alert alert-danger alert-dismissable err" id="err3">' +
                 '<button type="button" class="close" ' +
@@ -18,11 +34,11 @@ $(function () {
                 '</button>' +
                 'Valid email required' +
                 '</div>');
-            $('#err7').slideDown('slow');
+            $('#err2').slideDown('slow');
             console.log('email is invalid');
         }
         else {
-            $("#err3").slideUp('close');
-        });
-    }
-}
+            $("#err2").slideUp('close');
+        }
+    });
+});
