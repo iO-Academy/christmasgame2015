03a490610df807bd7be7f30b43b3142d548f003f
peter.wilkins.mayden <peter.wilkins@mayden.com>
2015-12-07 10:27:50 +0000

    clocks does hours

--- js/clock.js
+++ js/clock.js
@@ -3,8 +3,24 @@
  */
 $(function () {
     var ticks = 0;
-    var mins = 0;
     var clock;
+
+    function seconds2time(seconds) {
+        var hours = Math.floor(seconds / 3600);
+        var minutes = Math.floor((seconds - (hours * 3600)) / 60);
+        var seconds = seconds - (hours * 3600) - (minutes * 60);
+        var time = "";
+
+        if (hours != 0) {
+            time = hours + ":";
+        }
+
+            minutes = (minutes < 10 && time !== "") ? "0" + minutes : String(minutes);
+            time += minutes + ":";
+        time += (seconds < 10) ? "0" + seconds : String(seconds);
+
+        return time;
+    }
 
     $('#start').click(function () {
         $(document).trigger('start');
@@ -23,11 +39,11 @@ $(function () {
 
     $(document).on('start', function () {
         clock = setInterval(function () {
-            $('#seconds').text(ticks++);
-            if (ticks === 60) {
-                $('#minutes').text(++mins);
-                ticks = 0;
-            }
+            $('#seconds').text(seconds2time(++ticks));
+            //if (ticks === 60) {
+            //    $('#minutes').text(++mins);
+            //    ticks = 0;
+            //}
         }, 10);
     });
 
@@ -39,7 +55,8 @@ $(function () {
     $(document).on('reset', function () {
 
         clearInterval(clock);
-        $('#seconds').text(ticks = 0);
-        $('#minutes').text(mins = 0);
+        ticks = 0;
+        $('#seconds').text("0:00");
     });
+
 });
