2904ffa7aa799a9dd5b448c9ac118936d5d09bb3
charliecog <charlie.coggans@mayden.co.uk>
2015-12-07 10:27:16 +0000

    merging branch locally

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 	<head>
 		<link href='https://fonts.googleapis.com/css?family=Roboto:400,700' rel='stylesheet' type='text/css'>
 		<link type="text/css" href="css/layout.css" rel="stylesheet" media="all">
-
+		<link type="text/css" href="css/splash.css" rel="stylesheet" media="all">
 		<script src="https://code.jquery.com/jquery-1.9.1.min.js"></script>
         <script src="js/cookies.js"></script>
 		<script src="js/splash.js"></script>
@@ -10,7 +10,9 @@
 		<title>Mayden Christmas Game 2015</title>
 	</head>
 	<body>
-		<div id="game"></div>
+		<div id="game">
+            <?php include('templates/splash.html') ?>
+        </div>
 	</body>
 	<script>
 		//validating that the device is not touch screeng
