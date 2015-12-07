c1d887a8323e23471fc0a0c750837dd6996950e5
peter.wilkins.mayden <peter.wilkins@mayden.com>
2015-12-07 09:05:24 +0000

    timer events in clock.js

--- js/clock.js
+++ js/clock.js
@@ -1,13 +1,27 @@
 /**
  * Created by peterwilkins on 05/12/2015.
  */
-$(function() {
+$(function () {
     var ticks = 0;
     var mins = 0;
     var clock;
 
     $('#start').click(function () {
+        $(document).trigger('start');
         console.log("start clock");
+    });
+
+    $('#stop').click(function () {
+        $(document).trigger('stop');
+        console.log("stop clock");
+    });
+
+    $('#reset').click(function () {
+        console.log("reset clock");
+        $(document).trigger('reset');
+    });
+
+    $(document).on('start', function () {
         clock = setInterval(function () {
             $('#seconds').text(ticks++);
             if (ticks === 60) {
@@ -17,12 +31,13 @@ $(function() {
         }, 10);
     });
 
-    $('#stop').click(function () {
-        console.log("stop clock");
+
+    $(document).on('stop', function () {
         clearInterval(clock);
     });
-    $('#reset').click(function () {
-        console.log("reset clock");
+
+    $(document).on('reset', function () {
+
         clearInterval(clock);
         $('#seconds').text(ticks = 0);
         $('#minutes').text(mins = 0);
