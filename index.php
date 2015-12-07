7d8f99e8ea612a9854ad450c519f03c7177687a6
connorjones17 <connor.jones@mayden.co.uk>
2015-12-07 14:25:00 +0000

    removed gameVisual and added to index.php

--- index.php
+++ index.php
@@ -13,7 +13,23 @@
 	</head>
 	<body>
 		<div id="game">
+			<img src="img/game_background.png">
+			<div id="mazeContainer" class="gameLevelVisual">
+			</div>
 
+			<div id="message" class="gameLevelVisual">
+			</div>
+
+			<div id="timer" class="gameLevelVisual">
+				<?php include 'clock.html'; ?>
+			</div>
+
+			<div id="entryTally" class="gameLevelVisual">
+				<p>Attempts:</p>
+				<div id="tally">
+					01
+				</div>
+			</div>
         </div>
 	</body>
 	<script>
