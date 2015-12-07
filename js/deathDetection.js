aac225bdb54d856afad5bc8760d7872b3b6c7953
Ross E <ross.eastman@mayden.co.uk>
2015-12-07 10:33:34 +0000

    created death detection onlevels and death behaviour. See doc blox/todos for further info

---
+++ js/deathDetection.js
@@ -0,0 +1,45 @@
+$(function(){
+    /**
+     * //TODO .die class needs to be applied to all imgs which will induce a death
+     * //TODO set gameBoxDiv to mirror html
+     *
+     * 'death' custom event is used to trigger counter reset, and adds one to the attempts counter
+     *
+     * "pause" event = trigger to pause timer
+     * "addOneToAttempts" event = trigger to ad one to attempts counter
+     *
+     */
+
+    var $gameBoxDiv = ('enterGameBoxHTMLEntity')
+    var $messageDisplayBox = $('MDBdiv')
+    var commiserationsMessageAndInstructions = '<div>Failure HTML Here</div>'
+
+    /**
+     * custom trigger on mouse over divs for die behaviours
+     */
+
+    $('.die').mouseover(function() {
+        $gameBoxDiv.trigger('death')
+    })
+
+    /**
+     * Provides pop up and changes the message box contents.
+     *
+     * comment out/delete as required
+     *
+     * 'death' is the custom event
+     *
+     * 'pause' is the custom trigger for pausing the counter
+     *
+     * 'addOneToAttempts' is the custom trigger for adding one to the attempts counter
+     *
+     */
+
+    $gameBoxDiv.on('death', function(){
+        alert("You have died! Please try again! Click the start area to start")
+        $messageDisplayBox.replaceWith("'commiserationsMessageAndInstructions'")
+        $gameBoxDiv.trigger('pause')
+        $gameBoxDiv.trigger('addOneToAttempts')
+    })
+
+})
