b4e64e1c0e90cf7dd8f7ae8447a30e19b0c84fd0
peter.wilkins.mayden <peter.wilkins@mayden.com>
2015-12-07 18:42:01 +0000

    added timer script link to index.php

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 	<head>
 		<link href='https://fonts.googleapis.com/css?family=Roboto:400,700' rel='stylesheet' type='text/css'>
 		<link type="text/css" href="css/layout.css" rel="stylesheet" media="all">
+        <script src="js/timer.js" type="text/javascript"></script>
 		<title>Mayden Christmas Game 2015</title>
 	</head>
 	<body>
