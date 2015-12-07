60e28989ce4953efbcda13f0da11cd3ed48fad87
peter.wilkins.mayden <peter.wilkins@mayden.com>
2015-12-07 18:49:49 +0000

    fixed spaces index.php

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <html>
-	<head>
-		<link href='https://fonts.googleapis.com/css?family=Roboto:400,700' rel='stylesheet' type='text/css'>
-		<link type="text/css" href="css/layout.css" rel="stylesheet" media="all">
-        <script src="js/timer.js" type="text/javascript"></script>
-		<title>Mayden Christmas Game 2015</title>
-	</head>
-	<body>
-		<div id="game"></div>
-	</body>
+<head>
+    <link href='https://fonts.googleapis.com/css?family=Roboto:400,700' rel='stylesheet' type='text/css'>
+    <link type="text/css" href="css/layout.css" rel="stylesheet" media="all">
+    <script src="js/timer.js" type="text/javascript"></script>
+    <title>Mayden Christmas Game 2015</title>
+</head>
+<body>
+<div id="game"></div>
+</body>
 </html>
