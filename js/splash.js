ce3d7fd1893c1a0012edd0e0d086e01659bf1bbd
charliecog <charlie.coggans@mayden.co.uk>
2015-12-07 15:08:34 +0000

    fixed error message, abstracted validate into validate.js

--- js/splash.js
+++ js/splash.js
@@ -11,39 +11,9 @@ $(function () {
         e.preventDefault();
         var userName = $('#userName').val()
         var userEmail = $('#userEmail').val()
-        $('.err').remove()
-        if (userName.length == 0) {
-            $('#userName').after(
-                '<div class="err" id="err1">' +
-                'Required field' +
-                '</div>');
-            $('#err1').slideDown('slow');
-        }
 
-        if (userName.length > 100) {
-            $('#userName').after(
-                '<div class="err" id="err3">' +
-                'Max length is 100' +
-                '</div>');
-            $('#err3').slideDown('slow');
-        }
-        if(!validateName(userName)) {
-            $('#userName').after(
-                '<div class="err" id="err4">' +
-                'Valid characters only' +
-                '</div>');
-            $('#err4').slideDown('slow');
-        }
-
-        if (!validateEmail(userEmail)) {
-            $('#userEmail').after(
-                '<div class="err" id="err2">' +
-                'Valid email required' +
-                '</div>');
-            $('#err2').slideDown('slow');
-        }
         
-        else {
+        if(validate(userName, userEmail)) {
             $.post('api/index.php', {
                 'action': 'createUser',
                 'userName': userName,
