5d8978ad42fd5e3b8ff57bab421f7ab4cd7555ce
peter.wilkins.mayden <peter.wilkins@mayden.com>
2015-12-07 18:27:04 +0000

    changes for pull request, remove jquery

--- js/timer.js
+++ js/timer.js
@@ -1,39 +1,38 @@
-$(function () {
-    var ticks = 0;
-    var clock;
+var ticks = 0;
+var clock;
 
-    /**
-     * function seconds2time converts seconds to time string for display
-     * @param seconds
-     * @returns {string}
-     */
-    function seconds2time(seconds) {
-        var hours = Math.floor(seconds / 3600);
-        var minutes = Math.floor((seconds - (hours * 3600)) / 60);
-        var seconds = seconds - (hours * 3600) - (minutes * 60);
-        var time = "";
-        if (hours != 0) {
-            time = hours + ":";
-        }
-        minutes = (minutes < 10 && time !== "") ? "0" + minutes : String(minutes);
-        time += minutes + ":";
-        time += (seconds < 10) ? "0" + seconds : String(seconds);
-        return time;
+/**
+ * converts and formats seconds to hh:mm:ss string
+ * @number seconds
+ * @returns {string}
+ */
+function seconds2time(seconds) {
+    var hours = Math.floor(seconds / 3600);
+    var minutes = Math.floor((seconds - (hours * 3600)) / 60);
+    var seconds = seconds - (hours * 3600) - (minutes * 60);
+    var time = "";
+    if (hours != 0) {
+        time = hours + ":";
     }
+    minutes = (minutes < 10) ? "0" + minutes : String(minutes);
+    time += minutes + ":";
+    time += (seconds < 10) ? "0" + seconds : String(seconds);
+    return time;
+}
 
-    $(document).on('startClock', function () {
-        clock = setInterval(function () {
-            $('#seconds').text(seconds2time(++ticks));
-        }, 1000);
-    });
+function startClock() {
+    clock = setInterval(function () {
+        document.getElementById('#seconds').text(seconds2time(++ticks));
+    }, 1000);
+}
 
-    $(document).on('stopClock', function () {
-        clearInterval(clock);
-    });
+function stopClock() {
+    clearInterval(clock);
+}
 
-    $(document).on('resetClock', function () {
-        clearInterval(clock);
-        ticks = 0;
-        $('#seconds').text("0:00");
-    });
-});
+function resetClock() {
+    clearInterval(clock);
+    ticks = 0;
+    document.getElementById('#seconds').text("00:00");
+}
+
