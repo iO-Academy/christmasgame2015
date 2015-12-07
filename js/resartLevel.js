2eb436987e42252af4c2736af77231283a3bbf76
Ross E <ross.eastman@mayden.co.uk>
2015-12-07 10:59:48 +0000

    added level restart functionality, see doc blox and todos

--- js/resartLevel.js
+++ js/resartLevel.js
@@ -1,45 +1,32 @@
 $(function(){
     /**
-     * //TODO .die class needs to be applied to all imgs which will induce a death
-     * //TODO set gameBoxDiv to mirror html
+     * 'restartLevel' custom event is used to replace messagebox with instructions and prepare the game for the player to try again.
+     * 'restartLevel' is handled in deathDetection.js as there is no user restart level control
      *
-     * 'death' custom event is used to trigger counter reset, and adds one to the attempts counter
      *
-     * "pause" event = trigger to pause timer
-     * "addOneToAttempts" event = trigger to ad one to attempts counter
+     * //TODO set vars based on logic and html
+     * //TODO test manually
+     *
      *
      */
 
     var $gameBoxDiv = ('enterGameBoxHTMLEntity')
     var $messageDisplayBox = $('MDBdiv')
-    var commiserationsMessageAndInstructions = '<div>Failure HTML Here</div>'
+    var instructionsMessageAndInstructions = '<div>Instructions HTML Here</div>'
+    var $startBox = $('.#startBox')
 
     /**
-     * custom trigger on mouse over divs for die behaviours
-     */
-
-    $('.die').mouseover(function() {
-        $gameBoxDiv.trigger('death')
-    })
-
-    /**
-     * Provides pop up and changes the message box contents.
+     * changes the message box contents.
      *
-     * comment out/delete as required
-     *
-     * 'death' is the custom event
-     *
-     * 'pause' is the custom trigger for pausing the counter
-     *
-     * 'addOneToAttempts' is the custom trigger for adding one to the attempts counter
+     * 'start' is the custom event to start the timer
      *
      */
 
-    $gameBoxDiv.on('death', function(){
-        alert("You have died! Please try again! Click the start area to start")
-        $messageDisplayBox.replaceWith("'commiserationsMessageAndInstructions'")
-        $gameBoxDiv.trigger('pause')
-        $gameBoxDiv.trigger('addOneToAttempts')
+    $gameBoxDiv.on('restartLevel', function(){
+        $messageDisplayBox.replaceWith("'instructionsMessageAndInstructions'")
+        $startBox.click(function() {
+            $gameBoxDiv.trigger('start')
+        })
     })
 
 })
