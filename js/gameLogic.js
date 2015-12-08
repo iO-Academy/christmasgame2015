43c769cef3999b9414c8aa3ce82a9a23678e9a34
Ross E <ross.eastman@mayden.co.uk>
2015-12-08 10:03:38 +0000

    tidy up and split funcitons from calls

--- js/gameLogic.js
+++ js/gameLogic.js
@@ -9,112 +9,8 @@ var $startSafeZone = $('#startArea')
 var gameover = 2
 var genericError = "error, error"
 
-
-$(function () {
-    /**
-     * Game Logic!!
-     */
-
-    /**
-     *startGame funcitonality
-     */
-    /**
-     * ajax get request for next level and puts result into $gameBoxDiv
-     *
-     * @param levelNumber
-     */
-    function loadLevel(levelNumber) {
-        $.get(('templates/level' + levelNumber + '.html'),
-            function (data) {
-                $gameBoxDiv.replaceWith(data)
-                attemptsCount = 0
-                resetClock();
-            })
-    }
-
-    /**
-     * TODO change this $startSafeZone var to whatever the start element is
-     * @type {*|jQuery|HTMLElement}
-     */
-
-    $startSafeZone.click(function() {
-        //Start Clock
-        startClock();
-        //increase attempt counter by 1
-        $('#tally').text(++attemptsCount)
-        //disable start zone
-        $startSafeZone.off('click')
-        //triggers death event
-        $('.die').mouseover(function() {
-            $gameBoxDiv.trigger('death')
-        })
-        //triggers complete event
-        $finishSafeZone.mouseover(function() {
-            $gameBoxDiv.trigger('completedLevel')
-        })
-    })
-
-    /**
-     * Success functionality
-     */
-    $finishBox.mouseover(function() {
-        //todo remove post test
-        alert("You have completed level 'levelNumber'")
-        //change message box to display level congrats
-        $messageDisplayBox.replaceWith("'congratulationsMessage'")
-        //stops the clock
-        stopClock();
-        //disable death
-        $('.die').off('mouseover')
-        $.post('api/index.php', {
-                'action': 'saveLevel',
-                'level': levelNumber,
-                'attempts': attemptsCount,
-                'time': ticks
-            }, // put data here from ajax into endOfGame
-            function(data) {
-                //success function
-                levelNumber++
-                if (levelNumber === gameover) {
-                    $messageDisplayBox.replaceWith('#endOfGame').css({opacity: 0})
-                    $messageDisplayBox.animate({
-                        opacity: "100",
-                        width: "600px",
-                        height: "400px",
-                        right: "0px",
-                        top: "0px"
-                    })
-                }
-                else {
-                    loadLevel(levelNumber);
-                }
-            }
-        ).fail(function() {
-            $('#game').replaceWith(genericError);
-        })
-    })
-
-    $('.die').mouseover(function() {
-        $gameBoxDiv.trigger('death')
-    })
-    /*
-     * When the user dies this function is called.
-     * It does exactly what it says in the function, if you've got this far and cannot work out what this does then it's
-     * even more of a waste of my time explaining it for you.
-     * For now, proceed.
-     */
-    $gameBoxDiv.on('death', function () {
-        stopClock()
-        $startSafeZone.on('click')
-        $messageDisplayBox.replaceWith("You have died! Please try again! Click the start area to start")
-        $('.die').off('death')
-    })
-})
 /**
- *startGame funcitonality
- */
-/**
- * ajax get request for next level and puts result into $gameBoxDiv
+ * ajax request for next level
  *
  * @param levelNumber
  */
@@ -125,3 +21,79 @@ function loadLevel(levelNumber) {
             resetClock()
         })
 }
+
+
+function startGame() {
+//Start Clock
+    startClock();
+    //increase attempt counter by 1
+    $('#tally').text(++attemptsCount)
+    //disable start zone
+    $startSafeZone.off('click')
+
+    $('.die').on('death')
+}
+function finishGame() {
+//todo remove post test
+    alert("You have completed level 'levelNumber'")
+    //change message box to display level congrats
+    $messageDisplayBox.replaceWith("'congratulationsMessage'")
+    //stops the clock
+    stopClock();
+    //disable death
+    $('.die').off('mouseover')
+    $.post('api/index.php', {
+            'action': 'saveLevel',
+            'level': levelNumber,
+            'attempts': attemptsCount,
+            'time': ticks
+        }, // put data here from ajax into endOfGame
+        function (data) {
+            //success function
+            levelNumber++
+            if (levelNumber === gameover) {
+                $messageDisplayBox.replaceWith('#endOfGame').css({opacity: 0})
+                $messageDisplayBox.animate({
+                    opacity: "100",
+                    width: "600px",
+                    height: "400px",
+                    right: "0px",
+                    top: "0px"
+                })
+            }
+            else {
+                loadLevel(levelNumber);
+            }
+        }
+    ).fail(function () {
+        $('#game').replaceWith(genericError);
+    })
+}
+function gameDeath() {
+    stopClock()
+    $startSafeZone.on('click')
+    $messageDisplayBox.replaceWith("You have died! Please try again! Click the start area to start")
+    $('.die').off('death')
+}
+$(function () {
+
+    $startSafeZone.click(startGame())
+
+    $finishBox.mouseover(finishGame())
+    //triggers death event
+    $('.die').mouseover(function () {
+        $gameBoxDiv.trigger('death')
+    })
+    //triggers complete event
+    $finishSafeZone.mouseover(function () {
+        $gameBoxDiv.trigger('completedLevel')
+    })
+
+    $('.die').mouseover(function() {
+        $gameBoxDiv.trigger('death')
+    })
+
+    $gameBoxDiv.on('death', function () {
+        gameDeath()
+})
+
