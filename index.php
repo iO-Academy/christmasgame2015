b6a5aaf682780b873e93f4bd7640b98377874397
charliecog <charlie.coggans@mayden.co.uk>
2015-12-09 10:58:48 +0000

    changed css of message boxes

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
     </head>
     <body>
         <div id="game">
-            <?php include("templates/messages.html") ?>
+            <?php include("templates/splash.html") ?>
         </div>
     <script>
         //validating that the device is not touch screen
