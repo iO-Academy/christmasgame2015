5fdc4a4a5faaa47cfc98695082a7124bca518c67
charliecog <charlie.coggans@mayden.co.uk>
2015-12-09 15:56:13 +0000

    added moving present number 1 - uses clock timer

--- templates/level1.php
+++ templates/level1.php
@@ -42,7 +42,7 @@
 
 <div id="hanger1" class="levelImages"></div>
 <div id="hanger2" class="levelImages"></div>
-<div id="present_1_single" class="levelImages"></div>
+<div id="present_1_single" class="levelImages boundary"></div>
 <div id="present_1_gap" class="levelImages"></div>
 <div id="present_2_bottom" class="levelImages"></div>
 <div id="present_2_middle" class="levelImages"></div>
