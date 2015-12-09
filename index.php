251f7b8ace11bb17ee839f16e2922f5ac5b2cdbc
Lucia Velasco <lucia.velasco@mayden.co.uk>
2015-12-09 10:36:06 +0000

    first few divs

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
         <link href='https://fonts.googleapis.com/css?family=Roboto:400,100,700' rel='stylesheet' type='text/css'>
         <link type="text/css" href="css/layout.css" rel="stylesheet" media="all">
         <link type="text/css" href="css/splash.css" rel="stylesheet" media="all">
+        <link type="text/css" href="css/boundary.css" rel="stylesheet" media="all">
         <script src="https://code.jquery.com/jquery-1.9.1.min.js"></script>
         <script src="js/timer.js" type="text/javascript"></script>
         <script src="js/gameLogic.js" type="text/javascript"></script>
