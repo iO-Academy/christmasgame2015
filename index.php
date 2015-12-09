73922df922f374b17c0e46268f7dd106c721dd6c
charliecog <charlie.coggans@mayden.co.uk>
2015-12-09 10:28:43 +0000

    temp refix of box for marta

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
     </head>
     <body>
         <div id="game">
-            <?php include("templates/splash.html") ?>
+            <?php include("templates/messages.html") ?>
         </div>
     <script>
         //validating that the device is not touch screen
