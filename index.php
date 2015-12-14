ebd9bb4eafa4efc7c7436bd5b118f2e8cb530e98
mporam <emailme@mikeoram.co.uk>
2015-12-14 17:33:50 +0000

    minify JS and CSS

--- index.php
+++ index.php
@@ -4,18 +4,9 @@
     <head>
         <meta http-equiv="x-ua-compatible" content="IE-Edge"/>
         <link href='https://fonts.googleapis.com/css?family=Roboto:400,100,700' rel='stylesheet' type='text/css'>
-        <link type="text/css" href="css/splash.css" rel="stylesheet" media="all">
-        <link type="text/css" href="css/popup.css" rel="stylesheet" media="all">
-        <link type="text/css" href="css/layout.css" rel="stylesheet" media="all">
-        <link type="text/css" href="css/boundary.css" rel="stylesheet" media="all">
-        <link type="text/css" href="css/level1Images.css" rel="stylesheet" media="all">
+        <link type="text/css" href="css/main.min.css" rel="stylesheet" media="all">
         <script src="js/jquery-1.9.1.min.js"></script>
-        <script src="js/animate.js" type="text/javascript"></script>
-        <script src="js/timer.js" type="text/javascript"></script>
-        <script src="js/gameLogic.js" type="text/javascript"></script>
-        <script src="js/cookies.js" type="text/javascript"></script>
-        <script src="js/validate.js" type="text/javascript"></script>
-        <script src="js/splash.js" type="text/javascript"></script>
+        <script src="js/main.min.js" type="text/javascript"></script>
         <title>Mayden Christmas Game 2015</title>
     </head>
     <body>
